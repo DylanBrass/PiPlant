e1458c1e5bf7fbf54587b0605ec1767da5b621d7
dylanbrass <dylan.brassard@outlook.com>
2023-11-05 22:38:07 -0500

    Renamed files

---
+++ pi-plant-react-app/src/Pages/HomePage.jsx
@@ -0,0 +1,27 @@
+import Navbar from "./NavigationBar";
+import './HomePage.css';
+import axios from 'axios'
+const testGet = () =>{
+  axios
+        .get('http://'+window.location.hostname+':5000')
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+      })
+}
+
+
+function MainPage() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home Page</h1>
+      <p>This is the home page.</p>
+      <button onClick={()=> testGet()}>Test Get</button>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ pi-plant-react-app/src/Pages/NavigationBar.jsx
@@ -0,0 +1,26 @@
+// NavigationBar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavigationBar.css'; // Import the CSS file
+
+const NavigationBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/" className="app-name">
+            PiPlant
+          </Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
