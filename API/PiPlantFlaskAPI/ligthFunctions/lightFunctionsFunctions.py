053e381e22a44164d417feaa6732fa5571afa724
dylanbrass <dylan.brassard@outlook.com>
2023-11-07 22:46:32 -0500

    Adding light feature

--- API/PiPlantFlaskAPI/ligthFunctions/lightFunctionsFunctions.py
+++ API/PiPlantFlaskAPI/ligthFunctions/lightFunctionsFunctions.py
@@ -14,7 +14,7 @@ for pin in allLights.keys():
 
 def toggleLight(light: int):
     global allLights
-    if(light < len(allLights)):
+    if light - 1 < len(allLights):
         lightPin = list(allLights.keys())[light - 1]
         allLights[lightPin] = not allLights.get(lightPin)
         GPIO.output(lightPin, allLights.get(lightPin))
