605bd47622e4b16b8d45d9ef44db9c57abe2d16a
dylanbrass <dylan.brassard@outlook.com>
2023-11-11 16:43:31 -0500

    Adding date picker

--- pi-plant-react-app/src/Pages/ChartPage.jsx
+++ pi-plant-react-app/src/Pages/ChartPage.jsx
@@ -2,11 +2,14 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import Navbar from './NavigationBar';
+import DatePicker from "react-datepicker";
 
 
 function ChartPage() {
 
     const [data, setData] = useState([])
+
+    const [selectedDate, setSelectedDate] = useState(new Date().format("yyyy-MM-dd"));
 
     const fetchData = () => {
         axios.get('http://' + window.location.hostname + ':5000/getValuesForDay/2023-11-10/1')
@@ -26,7 +29,10 @@ function ChartPage() {
     return (
         <div>
             <Navbar />
-
+            <h1>Chart Page</h1>
+            <DatePicker selected={new Date()} onChange={(date) => console.log(date.format("yyyy-MM-dd"))} />
+            
+            
             <LineChart width={500} height={300} data={data}>
                 <XAxis dataKey="time" />
                 <YAxis dataKey="value" />
