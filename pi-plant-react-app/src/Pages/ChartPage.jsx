1ec9589b3034f37c40cc34d8f26951e264a48f37
dylanbrass <dylan.brassard@outlook.com>
2023-11-11 22:10:50 -0500

    Adding choosing date and sensor for chart

--- pi-plant-react-app/src/Pages/ChartPage.jsx
+++ pi-plant-react-app/src/Pages/ChartPage.jsx
@@ -52,35 +52,35 @@ function ChartPage() {
     return (
         <div>
             <Navbar />
-            <h1>Chart Page</h1>
-            
-          
-                <div className='chart'>
-                <LineChart width={700} height={500} data={data}>
-                    <XAxis dataKey="time"/>
-                    <YAxis dataKey="value" type="number"  domain={[0, 'dataMax']}/>
-                    <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
-                    <Line type="monotone" dataKey="value" stroke="#82ca9d" />
-                </LineChart>
-                </div>
-            
-            
-            <br/>
-            <div className='datepicker'>
-            <DatePicker
-                showIcon
-                dateFormat="yyyy-MM-dd"
-                selected={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-            />
-            <select
-                  onChange={(e) => setSelectedSensor(e.target.value)}
-                  defaultValue={selectedSensor}>
-                  {Array.from(Array(numberOfSensors).keys()).map((i) => (
-                    <option value={i + 1} key={i + 1}>{i + 1}</option>
-                  ))}
-                </select>
-                </div>
+                <center>
+                    <h1>Chart Page</h1>
+                    
+                
+                        <div className='chart'>
+                        <LineChart width={700} height={500} data={data}>
+                            <XAxis dataKey="time"/>
+                            <YAxis dataKey="value" type="number"  domain={[0, 'dataMax']}/>
+                            <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+                            <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+                        </LineChart>
+                        </div>
+                    <br/>
+                    <div className='datepicker'>
+                    <DatePicker
+                        showIcon
+                        dateFormat="yyyy-MM-dd"
+                        selected={selectedDate}
+                        onChange={(date) => setSelectedDate(date)}
+                    />
+                    <select
+                        onChange={(e) => setSelectedSensor(e.target.value)}
+                        defaultValue={selectedSensor}>
+                        {Array.from(Array(numberOfSensors).keys()).map((i) => (
+                            <option value={i + 1} key={i + 1}>{i + 1}</option>
+                        ))}
+                        </select>
+                    </div>
+                </center>
         </div>
     )
 }
