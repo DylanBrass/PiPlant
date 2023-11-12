1bfd4713a602b2a2b74d3875a5da66dc53a8d25f
dylanbrass <dylan.brassard@outlook.com>
2023-11-11 21:29:54 -0500

    Adding choosing date and sensor for chart

--- pi-plant-react-app/src/Pages/ChartPage.jsx
+++ pi-plant-react-app/src/Pages/ChartPage.jsx
@@ -57,8 +57,8 @@ function ChartPage() {
           
                 <div className='chart'>
                 <LineChart width={700} height={500} data={data}>
-                    <XAxis dataKey="time" type="number"  domain={['auto', 'auto']} />
-                    <YAxis dataKey="value" type="number"  domain={['auto', 'auto']}/>
+                    <XAxis dataKey="time" type="number" />
+                    <YAxis dataKey="value" type="number"  domain={[0, 'auto']}/>
                     <CartesianGrid stroke="#eee" strokeDasharray="3,3"/>
                     <Line type="monotone" dataKey="value" stroke="#82ca9d" />
                 </LineChart>
