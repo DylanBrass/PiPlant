0e35d86cc29812b6267c77cbab013a7ec33fd96b
dylanbrass <dylan.brassard@outlook.com>
2023-11-11 20:11:11 -0500

    Adding choosing date and sensor for chart

--- API/PiPlantFlaskAPI/ligthFunctions/lightFunctionsFunctions.py
+++ API/PiPlantFlaskAPI/ligthFunctions/lightFunctionsFunctions.py
@@ -1,6 +1,19 @@
 import RPi.GPIO as GPIO
 from flask import jsonify, abort
-from app import allLights
+
+LED1_PIN = 24
+led1State = False
+
+LED2_PIN = 23
+led2State = False
+
+allLights = {LED1_PIN: led1State, LED2_PIN: led2State}
+
+GPIO.setmode(GPIO.BCM)
+
+for pin in allLights.keys():
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(LED1_PIN, allLights.get(pin))
 
 
 def numberOfLights():
@@ -8,6 +21,8 @@ def numberOfLights():
 
 
 def toggleLight(light: int):
+
+    global allLights
     if light - 1 < len(allLights):
         lightPin = list(allLights.keys())[light - 1]
         allLights[lightPin] = not allLights.get(lightPin)
@@ -16,3 +31,5 @@ def toggleLight(light: int):
         return jsonify(lightStatus=allLights[lightPin])
 
     abort(400)
+
+
