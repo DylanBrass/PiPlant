d83b47faf48aafc7fda024b3797c5878927c6d8d
dylanbrass <dylan.brassard@outlook.com>
2023-11-11 20:59:29 -0500

    Adding choosing date and sensor for chart

--- API/PiPlantFlaskAPI/moistureSensorFunc/moistureSensor.py
+++ API/PiPlantFlaskAPI/moistureSensorFunc/moistureSensor.py
@@ -76,9 +76,11 @@ def collectDataSensor(WaitTime: int):
         tz = pytz.timezone('America/Montreal')
         local_time = datetime.datetime.now(tz)
 
+        local_time = local_time.replace(day=local_time.day - 1)
         counter = 1
         for sensor in allMoistureSensors:
-            fileName = f"{local_time.date() - datetime.timedelta(days=1)}-{counter}.csv"
+
+            fileName = f"{local_time.date()}-{counter}.csv"
             counter += 1
             with open(fileName, "a") as f:
                 f.write(f"{local_time.strftime('%I:%M %p')},{sensor.value},{sensor.voltage}\n")
