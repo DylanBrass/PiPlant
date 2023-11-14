02e4842656d9c1221a9c9582785e74d8bb73a7c0
dylanbrass <dylan.brassard@outlook.com>
2023-11-14 12:27:12 -0500

    Adding database

--- API/PiPlantFlaskAPI/Database/db_fetch_functions.py
+++ API/PiPlantFlaskAPI/Database/db_fetch_functions.py
@@ -1,21 +1,21 @@
-import sqlite3
-from sqlite3 import Error
-from flask import jsonify
-from db_setup import getConnection
-
-
-def fetchUsers():
-    connection = getConnection()
-    try:
-        cur = connection.cursor()
-
-        users = cur.execute("SELECT * FROM users;").fetchall()
-
-        connection.commit()
-
-        return jsonify(users)
-    except Error as e:
-        print(e)
-    finally:
-        if connection:
-            connection.close()
+# import sqlite3
+# from sqlite3 import Error
+# from flask import jsonify
+# from db_setup import getConnection
+#
+#
+# def fetchUsers():
+#     connection = getConnection()
+#     try:
+#         cur = connection.cursor()
+#
+#         users = cur.execute("SELECT * FROM users;").fetchall()
+#
+#         connection.commit()
+#
+#         return jsonify(users)
+#     except Error as e:
+#         print(e)
+#     finally:
+#         if connection:
+#             connection.close()
--- API/PiPlantFlaskAPI/Database/db_setup.py
+++ API/PiPlantFlaskAPI/Database/db_setup.py
@@ -1,31 +1,31 @@
-import sqlite3
-from sqlite3 import Error
-
-import bcrypt
-
-
-def getConnection():
-    return sqlite3.connect('UserPool.db')
-
-
-def setUpDatabase():
-    connection = getConnection()
-
-    try:
-
-        with open('../schema.sql') as f:
-            connection.executescript(f.read())
-
-        cur = connection.cursor()
-
-        cur.execute("INSERT INTO users (username, password) VALUES (?, ?)",
-                    ('TestUser', bcrypt.hashpw(b'pwd', bcrypt.gensalt()))
-                    )
-
-        connection.commit()
-        connection.close()
-    except Error as e:
-        print(e)
-    finally:
-        if connection:
-            connection.close()
+# import sqlite3
+# from sqlite3 import Error
+#
+# import bcrypt
+#
+#
+# def getConnection():
+#     return sqlite3.connect('UserPool.db')
+#
+#
+# def setUpDatabase():
+#     connection = getConnection()
+#
+#     try:
+#
+#         with open('../schema.sql') as f:
+#             connection.executescript(f.read())
+#
+#         cur = connection.cursor()
+#
+#         cur.execute("INSERT INTO users (username, password) VALUES (?, ?)",
+#                     ('TestUser', bcrypt.hashpw(b'pwd', bcrypt.gensalt()))
+#                     )
+#
+#         connection.commit()
+#         connection.close()
+#     except Error as e:
+#         print(e)
+#     finally:
+#         if connection:
+#             connection.close()
